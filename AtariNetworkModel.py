c9a28ced910428133bc1c21677414599a24ed4d4
benbotto <benbotto@users.noreply.github.com>
2018-02-08 17:35:32 -0800

    Network model for atari.

---
+++ AtariNetworkModel.py
@@ -0,0 +1,43 @@
+import numpy as np
+import tensorflow as tf
+from NetworkModel import NetworkModel
+
+class AtariNetworkModel(NetworkModel):
+  '''
+   ' Init.
+  '''
+  def __init__(self, model_file_name, env, learn_rate=0.00025):
+    NetworkModel.__init__(self, model_file_name, env, learn_rate)
+
+    self.stacked_frames  = 4
+    self.frame_width     = int(self.obs_shape[0] / 2)
+    self.frame_height    = int(self.obs_shape[1] / 2)
+
+  '''
+   ' Get the input shape.
+  '''
+  def get_input_shape(self):
+    return (self.stacked_frames, self.frame_width, self.frame_height)
+
+  '''
+   ' Build the model.
+  '''
+  def build(self):
+    print('Building model.')
+
+    # Build the model, as defined by the Nature paper on DQN by
+    # David Silver et. al.
+    self.network = tf.keras.models.Sequential()
+
+    self.network.add(tf.keras.layers.Lambda(lambda x: x / 255.0, input_shape=self.get_input_shape()))
+    self.network.add(tf.keras.layers.Conv2D(filters=32, kernel_size=8, strides=4, activation="relu", data_format="channels_first"))
+    self.network.add(tf.keras.layers.Conv2D(filters=64, kernel_size=4, strides=2, activation="relu"))
+    self.network.add(tf.keras.layers.Conv2D(filters=64, kernel_size=3, strides=1, activation="relu"))
+    self.network.add(tf.keras.layers.Flatten())
+    self.network.add(tf.keras.layers.Dense(512, activation="relu"))
+    self.network.add(tf.keras.layers.Dense(self.act_size, activation="linear"))
+
+    opt = tf.keras.optimizers.RMSprop(lr=self.learn_rate)
+
+    self.network.compile(loss="mean_squared_error", optimizer=opt)
+
