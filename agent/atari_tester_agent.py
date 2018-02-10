78bd3c230ca3920960b7bba1123ac437b2e106c6
benbotto <benbotto@users.noreply.github.com>
2018-02-09 18:22:48 -0800

    Atari tester agent to test weights.

---
+++ agent/atari_tester_agent.py
@@ -0,0 +1,24 @@
+import numpy as np
+from agent.tester_agent import TesterAgent
+from util.image_utils import preprocess_frame
+from util.frame_buffer import FrameBuffer
+
+class AtariTesterAgent(TesterAgent):
+  '''
+   ' Init.
+  '''
+  def __init__(self, env, model):
+    TesterAgent.__init__(self, env, model)
+
+    # Store and stacks the frames for easy access.  The DQN paper use the last
+    # four frames as an observation.
+    self._frames = FrameBuffer(self._model.stacked_frames)
+
+  '''
+   ' Process an observation.  They are converted to grayscale, scaled down by
+   ' a factor of two, then stacked.
+  '''
+  def process_obs(self, obs):
+    return self._frames.add_frame(
+      preprocess_frame(obs, self._model.frame_width, self._model.frame_height))
+
