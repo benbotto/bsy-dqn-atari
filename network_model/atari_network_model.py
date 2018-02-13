20712fe3b894747a87b77c46c5212fe42a50500b
benbotto <benbotto@users.noreply.github.com>
2018-02-12 16:51:48 -0800

    Switched to adam optimizer and huber loss.

--- network_model/atari_network_model.py
+++ network_model/atari_network_model.py
@@ -1,6 +1,7 @@
 import numpy as np
 import tensorflow as tf
 from network_model.network_model import NetworkModel
+from network_model.loss import huber_loss
 
 class AtariNetworkModel(NetworkModel):
   '''
@@ -37,7 +38,7 @@ class AtariNetworkModel(NetworkModel):
     self.network.add(tf.keras.layers.Dense(512, activation="relu"))
     self.network.add(tf.keras.layers.Dense(self.act_size, activation="linear"))
 
-    opt = tf.keras.optimizers.RMSprop(lr=self.learn_rate)
+    opt = tf.keras.optimizers.Adam(lr=self.learn_rate)
 
-    self.network.compile(loss="mean_squared_error", optimizer=opt)
+    self.network.compile(loss=huber_loss, optimizer=opt)
 
