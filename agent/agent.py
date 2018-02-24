73adc90e7abbe3c03efb5fe8b9b36e8cfec79da9
Ben Botto <benjamin.botto@gmail.com>
2018-02-24 07:37:56 -0800

    Added random no-ops at start, fire to start game.

--- agent/agent.py
+++ agent/agent.py
@@ -22,6 +22,25 @@ class Agent(ABC):
     # For keeping track of the maximum reward over time.
     self._max_reward = -1000000
 
+    # Number of random no-ops when a game starts.
+    self.start_no_ops = 10
+
+    # Helper actions for starting the game (see reset() below).
+    act_meanings = self._env.unwrapped.get_action_meanings()
+
+    self._no_op_act = None
+    self._fire_act  = None
+
+    for i in range(len(act_meanings)):
+      if act_meanings[i] == 'NOOP':
+        self._no_op_act = i
+      if act_meanings[i] == 'FIRE':
+        self._fire_act = i
+
+    # Some games, like Breakout, have a life counter.  Lives are kept
+    # track of.
+    self.lives = 0
+
   '''
    ' Epsilon-greedy policy is used.
   '''
@@ -42,16 +61,47 @@ class Agent(ABC):
     return np.sign(reward)
 
   '''
-   ' Reset the environment.
+   ' Reset the environment.  If there is a 'FIRE' action it is sent to start
+   ' the game.
+   ' Initially a series of no-ops are sent to make the environment stochastic.
   '''
   def reset(self):
-    return self._env.reset()
+    done       = True
+    obs        = self._env.reset()
+    self.lives = 0
+
+    while done:
+      # Fire to start the game.
+      if self._fire_act is not None:
+        obs, _, done, _ = self._env.step(self._fire_act)
+
+      # Send a random number of no-ops.
+      if self._no_op_act is not None and not done:
+        num_no_ops = np.random.randint(0, self.start_no_ops)
+
+        for i in range(num_no_ops):
+          obs, _, done, _ = self._env.step(self._no_op_act)
+
+          if done:
+            break
+
+    return obs
 
   '''
    ' Step the environment.
   '''
   def step(self, action):
-    return self._env.step(action)
+    new_obs, reward, done, info = self._env.step(action)
+
+    # For games that have a life counter, losing a life is considered terminal.
+    # This is how the Nature paper on DQN trained.
+    if 'ale.lives' in info:
+      if info['ale.lives'] < self.lives and self._fire_act is not None:
+        new_obs, reward, done, info = self._env.step(self._fire_act)
+
+      self.lives = info['ale.lives']
+
+    return new_obs, reward, done, info
 
   '''
    ' Track episode rewards.
