c62e5110e62b8e8f77d0616b9ded714b9b4feaac
Ben Botto <benjamin.botto@gmail.com>
2018-02-27 17:21:34 -0800

    Agent refactored to use new wrapped environments.  The agent is now responsible for controlling the env only, and doesn't deal with things like starting, episodic life, no-op starts, image processing, etc.

--- agent/agent.py
+++ agent/agent.py
@@ -22,36 +22,12 @@ class Agent(ABC):
     # For keeping track of the maximum reward over time.
     self._max_reward = -1000000
 
-    # Number of random no-ops when a game starts.
-    self.start_no_ops = 10
-
-    # Helper actions for starting the game (see reset() below).
-    act_meanings = self._env.unwrapped.get_action_meanings()
-
-    self._no_op_act = None
-    self._fire_act  = None
-
-    for i in range(len(act_meanings)):
-      if act_meanings[i] == 'NOOP':
-        self._no_op_act = i
-      if act_meanings[i] == 'FIRE':
-        self._fire_act = i
-
-    # Some games, like Breakout, have a life counter.  Lives are kept
-    # track of.
-    self.lives = 0
-
   '''
-   ' Epsilon-greedy policy is used.
+   ' Get epsilon, which may be based on timestep.
   '''
+  @abstractmethod
   def get_epsilon(self, total_t):
-    return .005
-
-  '''
-   ' Process an observation.
-  '''
-  def process_obs(self, obs):
-    return obs
+    pass
 
   '''
    ' Process a reward.
@@ -59,48 +35,6 @@ class Agent(ABC):
   '''
   def process_reward(self, reward):
     return np.sign(reward)
-
-  '''
-   ' Reset the environment.  If there is a 'FIRE' action it is sent to start
-   ' the game.
-   ' Initially a series of no-ops are sent to make the environment stochastic.
-  '''
-  def reset(self):
-    done       = True
-    obs        = self._env.reset()
-    self.lives = 0
-
-    while done:
-      # Fire to start the game.
-      if self._fire_act is not None:
-        obs, _, done, _ = self._env.step(self._fire_act)
-
-      # Send a random number of no-ops.
-      if self._no_op_act is not None and not done:
-        num_no_ops = np.random.randint(0, self.start_no_ops)
-
-        for i in range(num_no_ops):
-          obs, _, done, _ = self._env.step(self._no_op_act)
-
-          if done:
-            break
-
-    return obs
-
-  '''
-   ' Step the environment.
-  '''
-  def step(self, action):
-    new_obs, reward, done, info = self._env.step(action)
-
-    # The fire key needs to be pressed after a life is lost.
-    if 'ale.lives' in info:
-      if info['ale.lives'] < self.lives and self._fire_act is not None:
-        new_obs, reward, done, info = self._env.step(self._fire_act)
-
-      self.lives = info['ale.lives']
-
-    return new_obs, reward, done, info
 
   '''
    ' Track episode rewards.
