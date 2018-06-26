bfa5702eb3ab3dabe56eabb3a56f9b2ece55b80a
Ben Botto <benjamin.botto@gmail.com>
2018-06-26 07:48:00 -0700

    Huber loss changed to multiply the loss by the error before gradient clipping.

--- network_model/loss.py
+++ network_model/loss.py
@@ -2,32 +2,19 @@ import numpy as np
 import tensorflow as tf
 
 '''
- ' Huber loss.
+ ' Huber loss, importance sampling weighted.
  ' http://srome.github.io/A-Tour-Of-Gotchas-When-Implementing-Deep-Q-Networks-With-Keras-And-OpenAi-Gym/
  ' https://jaromiru.com/2017/05/27/on-using-huber-loss-in-deep-q-learning/
  ' https://en.wikipedia.org/wiki/Huber_loss
- ' The first link has a nice math trick to avoid a tf.where.
+ ' The first link has a nice math trick to avoid a tf.keras.backend.switch.
 '''
-def huber_loss(y_true, y_pred, clip_delta=1.0):
-  error = y_true - y_pred
-  cond  = tf.keras.backend.abs(error) < clip_delta
+def huber_loss_mean_weighted(y_true, y_pred, is_weights):
+  error = tf.keras.backend.abs((y_true - y_pred) * is_weights)
+  cond  = error <= 1.0
 
   squared_loss = 0.5 * tf.keras.backend.square(error)
-  linear_loss  = clip_delta * (tf.keras.backend.abs(error) - 0.5 * clip_delta)
+  linear_loss  = error - 0.5
 
-  return tf.where(cond, squared_loss, linear_loss)
-
-'''
- ' Same as above but returns the mean loss.
-'''
-def huber_loss_mean(y_true, y_pred, clip_delta=1.0):
-  return tf.keras.backend.mean(huber_loss(y_true, y_pred, clip_delta))
-
-'''
- ' Importance Sampling weighted huber loss.
-'''
-def huber_loss_mean_weighted(y_true, y_pred, is_weights, clip_delta=1.0):
-  error = huber_loss(y_true, y_pred, clip_delta)
-
-  return tf.keras.backend.mean(error * is_weights)
+  return tf.keras.backend.mean(
+    tf.keras.backend.switch(cond, squared_loss, linear_loss))
 
